b4617bb89c8f024a3be35d121942ba068939f040
Fulvio <fulviocanducci@hotmail.com>
2019-07-25 12:17:37 -0300

    Add SubscriptionBody

---
+++ Canducci.Recurrence/SubscriptionBody.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Canducci.Recurrence
+{
+    
+    public class SubscriptionItem
+    {
+        public SubscriptionItem(string name, int amount, decimal value)
+        {
+            Name = name;
+            Amount = amount;
+            Value = value;
+        }
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new System.FormatException("Mínimo de 1 caractere e máximo de 255 caracteres.");
+                }
+                if (value.Length <= 0 || value.Length > 255)
+                {
+                    throw new System.FormatException("Mínimo de 1 caractere e máximo de 255 caracteres.");
+                }
+                name = value;
+
+            }
+        }
+        private int amount;
+        public int Amount
+        {
+            get
+            {
+                return amount;
+            }
+            set
+            {
+                if (value <=0)
+                {
+                    throw new FormatException("Mínimo de 1 (Integer)");
+                }
+                amount = value;
+            }
+        }
+        private decimal value;
+        public decimal Value
+        {
+            get
+            {
+                return value;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new FormatException("Mínimo de 1 real");
+                }
+                this.value = value;
+            }
+        }
+        public dynamic ToObject()
+        {
+            return new
+            {
+                name = Name,
+                value = (Value * 100),
+                amount = Amount
+            };
+        }
+    }
+    public class SubscriptionBody: 
+        List<SubscriptionItem>, 
+        IList<SubscriptionItem>
+    {
+        public IEnumerable<dynamic> ToObjects()
+        {
+            foreach (SubscriptionItem item in this.ToList())
+            {
+                yield return item.ToObject();
+            }
+        }
+    }
+}
